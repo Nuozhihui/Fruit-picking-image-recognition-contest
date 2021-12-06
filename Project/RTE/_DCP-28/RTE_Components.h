fa528dd1917b76c63e6b99e225b83d8bcf1ac935
Nuozhihui <49518571+Nuozhihui@users.noreply.github.com>
2021-12-06 12:17:27 +0800

    基础上传
    基础上传

---
+++ Project/RTE/_DCP-28/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'DCP-28' 
+ * Target:  'DCP-28' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
